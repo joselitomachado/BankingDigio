5d117c9fe570c5fc7c2f2635e2699e25ea2d1f4a
Joselito Machado <opaguelosantos@gmail.com>
2024-02-20 20:10:43 -0300

    feat: add verificação de CPF existente

--- src/BankingDigio/Models/Conta.cs
+++ src/BankingDigio/Models/Conta.cs
@@ -64,5 +64,15 @@
         {
             return this.Movimentacoes;
         }
+
+        public static bool ValidarCPF(string cpf)
+        {
+            return cpf.Length == 6;
+        }
+
+        public static bool ValidarSenha(string senha)
+        {
+            return senha.Length >= 6 && senha.Length <= 16;
+        }
     }
 }
